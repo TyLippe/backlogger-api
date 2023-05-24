6b8f7ec1fad31099f7194d05d266f5926dee5062
Ty <tylippe94@gmail.com>
2023-05-24 10:40:45 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+const cors = require('cors');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cors({ origin: true }));
+
+  await app.listen(3000);
+}
+bootstrap();
